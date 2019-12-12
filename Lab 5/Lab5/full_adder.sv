6035c59d728a2dd439b543cb4258a0c3a694deec
Pouya <pouya.akbar@gmail.com>
2019-12-11 22:13:01 -0600

    Removed all code, added all reports

--- Lab 5/Lab5/full_adder.sv
+++
@@ -1,9 +0,0 @@
-module full_adder
-(
-	input x,y,z,
-	output sum, carry_out);
-	
-	assign sum = x ^ y ^z;
-	assign carry_out = (x&y) | (y&z) | (x&z);
-	
-endmodule
--- Lab 6/sv/adder.sv
+++
@@ -1,7 +0,0 @@
-module adder(input logic [15:0] ADDR2_OUT, ADDR1_OUT,
-			output logic [15:0] out);
-		always_comb
-		begin
-			out <= ADDR2_OUT+ADDR1_OUT;
-		end
-endmodule
